26982d3547a24e1165e8200712d05e9083c7530c
Valerian de Thezan de Gaussan <valerian@dtdg.fr>
2026-05-30 18:08:39 +0200

    Add discreet footer link to dtdg.fr

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,17 @@
 </script>
 
 <main
-	class="flex min-h-dvh items-center justify-center"
+	class="flex min-h-dvh flex-col items-center justify-center"
 	style="background: {COLORS.background}"
 >
 	<GameCanvas />
+	<footer class="py-2">
+		<a
+			href="https://dtdg.fr"
+			class="text-xs opacity-40 transition-opacity hover:opacity-70"
+			style="color: {COLORS.ball1}"
+		>
+			made by vdtdg
+		</a>
+	</footer>
 </main>
