8cbd5900ebae1dc0c23a7b8311dba6856f7f44af
Valerian de Thezan de Gaussan <valerian@dtdg.fr>
2026-05-30 16:40:36 +0200

    Lower brightness on territory and grid colors

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,14 +1,14 @@
 export const GRID_SIZE = 20;
 export const TICK_RATE = 60;
-export const BALL_SPEED = 6 / TICK_RATE;
+export const BALL_SPEED = 60 / TICK_RATE;
 export const PIXEL_SIZE = 28;
 
 export const COLORS = {
 	background: '#111128',
-	player1: '#d94fa0',
-	player2: '#4fd9c8',
-	gridLine: '#3a3a7a',
-	gridGlow: '#5a4afa',
+	player1: '#b04080',
+	player2: '#40b0a0',
+	gridLine: '#2a2a55',
+	gridGlow: '#4030cc',
 	ball1: '#ff80b8',
 	ball2: '#80ffd0',
 	ball1Glow: '#d94fa0',
